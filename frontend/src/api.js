be1b646c8b6502fc5e6d2022bb6bea4e63df2b8a
Matus Muransky <matus.muransky2@gmail.com>
2020-10-13 09:26:25 +0200

    Axios base URL changed to backend service at prod server

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import Axios from 'axios';
 
 const api = Axios.create({
-	baseURL: 'http://localhost:5000/api/'
+	//baseURL: 'http://localhost:5000/api/'
+	baseURL: 'http://158.195.36.37:5000/api/'
 });
 
 export default api;
