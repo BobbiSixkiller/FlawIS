a03cf7233b975c80f0c13d2c4f045369dcc72797
Matus Muransky <matus.muransky2@gmail.com>
2020-10-14 23:11:39 +0200

    Changed axios baseURL to use https

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import Axios from 'axios';
 
 const api = Axios.create({
 	//baseURL: 'http://localhost:5000/api/'
-	baseURL: 'http://flawis-backend.flaw.uniba.sk/api/'
+	baseURL: 'https://flawis-backend.flaw.uniba.sk/api/'
 });
 
 export default api;
