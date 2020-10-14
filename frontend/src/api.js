1f5a1ef2546eaf67837a26c5a24a7ce24d82348d
Matus Muransky <matus.muransky2@gmail.com>
2020-10-14 13:34:08 +0200

    API base url set to DNS record of backend service

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import Axios from 'axios';
 
 const api = Axios.create({
 	//baseURL: 'http://localhost:5000/api/'
-	baseURL: 'http://158.195.36.37:5000/api/'
+	baseURL: 'http://flawis-backend.flaw.uniba.sk/api/'
 });
 
 export default api;
