fcf50ea45184154fcfbd4a081ae30f63e2b44ef9
Matus Muransky <matus.muransky2@gmail.com>
2021-05-04 10:39:23 +0200

    minor verifyToken refactor

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -1,19 +1,22 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
-const mongoose = require('mongoose');
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const mongoose = require("mongoose");
 
 //middleware function pre auth-token check
-module.exports = async function(req, res, next) {
-	const token = req.header('authToken');
-	if (!token) return res.status(401).send({error: "Prístup zamietnutý, prosím prihláste sa!"});
+module.exports = async function (req, res, next) {
+	const token = req.header("authToken");
+	if (!token)
+		return res
+			.status(401)
+			.send({ error: "Prístup zamietnutý, prosím prihláste sa!" });
 
 	try {
-		//vrati rozhashovane user._id 
-		const id = await jwt.verify(token, process.env.SECRET);
-		req.user = await User.find({_id: id, 'tokens.token': token});
+		//vrati rozhashovane user._id
+		const id = jwt.verify(token, process.env.SECRET);
+		req.user = await User.find({ _id: id, "tokens.token": token });
 		req.token = token;
 		next();
-	} catch(err) {
-		res.status(401).send({error: err.message});
+	} catch (err) {
+		res.status(401).send({ error: err.message });
 	}
-}
+};
