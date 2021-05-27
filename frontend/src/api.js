00addb53d60f3c61343eddca08ebe6163fcd7061
Matus Muransky <matus.muransky2@gmail.com>
2021-05-27 09:03:10 +0200

    Merging with dev

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,11 +1,7 @@
 import Axios from "axios";
 
 const api = Axios.create({
-<<<<<<< HEAD
-	baseURL: 'https://flawis-backend.flaw.uniba.sk/api/'
-=======
-	baseURL: "http://localhost:5000/api/",
->>>>>>> dev
+	baseURL: "https://flawis-backend.flaw.uniba.sk/api/",
 });
 
 export default api;
