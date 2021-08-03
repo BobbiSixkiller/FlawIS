067446a9005dce2f0219ad6162d7c8d745c1f3c0
Matus Muransky <matus.muransky2@gmail.com>
2021-08-03 13:39:43 +0200

    Merging with dev

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,9 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-<<<<<<< HEAD
-import { UserProvider } from "./hooks/useUser";
-
-import Register from "./components/user/Register";
-import Login from "./components/user/Login";
-import ForgotPassword from "./components/user/ForgotPassword";
-import PasswordReset from "./components/user/PasswordReset";
-=======
 import { AuthRoute } from "./util/AuthRoute";
 import { AuthProvider } from "./context/auth";
 
->>>>>>> dev
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
@@ -25,22 +16,15 @@ import Footer from "./components/Footer";
 import { Container } from "reactstrap";
 
 function App() {
-<<<<<<< HEAD
 	return (
-		<UserProvider>
+		<AuthProvider>
 			<BrowserRouter>
-				<Switch>
-					<div id="App" className="d-flex flex-column justify-content-between">
-						<Header />
-						<Container className="my-5">
+				<div id="App" className="d-flex flex-column justify-content-between">
+					<Header />
+					<Container className="my-5">
+						<Switch>
 							<Route exact path="/">
 								<Home />
-							</Route>
-							<Route path="/mygrants">
-								<MyGrants />
-							</Route>
-							<Route path="/posts">
-								<Posts />
 							</Route>
 							<Route path="/register">
 								<Register />
@@ -54,53 +38,16 @@ function App() {
 							<Route path="/resetPassword/:token">
 								<PasswordReset />
 							</Route>
-							<Route path="/grants">
-								<Grants />
-							</Route>
-							<Route path="/users">
-								<Users />
-							</Route>
-						</Container>
-						<Footer />
-					</div>
-				</Switch>
+							<AuthRoute path="/dashboard">
+								<Dashboard />
+							</AuthRoute>
+						</Switch>
+					</Container>
+					<Footer />
+				</div>
 			</BrowserRouter>
-		</UserProvider>
+		</AuthProvider>
 	);
-=======
-  return (
-    <AuthProvider>
-      <BrowserRouter>
-        <div id="App" className="d-flex flex-column justify-content-between">
-          <Header />
-          <Container className="my-5">
-            <Switch>
-              <Route exact path="/">
-                <Home />
-              </Route>
-              <Route path="/register">
-                <Register />
-              </Route>
-              <Route path="/login">
-                <Login />
-              </Route>
-              <Route path="/forgotPassword">
-                <ForgotPassword />
-              </Route>
-              <Route path="/resetPassword/:token">
-                <PasswordReset />
-              </Route>
-              <AuthRoute path="/dashboard">
-                <Dashboard />
-              </AuthRoute>
-            </Switch>
-          </Container>
-          <Footer />
-        </div>
-      </BrowserRouter>
-    </AuthProvider>
-  );
->>>>>>> dev
 }
 
 export default App;
