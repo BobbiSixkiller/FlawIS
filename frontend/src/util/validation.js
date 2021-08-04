e1bd3b8b5134818670bde4e2d136ef20f85d352f
Matus Muransky <matus.muransky2@gmail.com>
2021-08-04 15:31:54 +0200

    Formik implementation in progress

--- frontend/src/util/validation.js
+++ frontend/src/util/validation.js
@@ -40,33 +40,6 @@ export function validateDocument(values) {
 	return { errors, valid };
 }
 
-export function validatePost(values) {
-	let errors = {};
-	let valid = {};
-
-	if (!values.name) {
-		errors.name = "Zadajte názov oznamu!";
-	} else {
-		valid.name = "Názov oznamu OK";
-	}
-
-	if (!values.body) {
-		errors.body = "Zadajte text oznamu!";
-	} else if (values.body.length > 3000) {
-		errors.body = "Text oznamu nesmie byť dlhší ako 3000 znakov!";
-	} else {
-		valid.body = "Oznam OK";
-	}
-
-	if (values.tags.length === 0) {
-		errors.tags = "Zadajte aspoň jednu oblasť príspevku!";
-	} else {
-		valid.tags = "Tagy OK";
-	}
-
-	return { errors, valid };
-}
-
 const Yup = require("yup");
 
 export const userSchema = Yup.object({
