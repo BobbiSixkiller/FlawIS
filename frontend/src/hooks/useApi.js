b40226a038ec7a8c67955124e8e93e343a4e22ee
Matus Muransky <matus.muransky2@gmail.com>
2021-08-24 11:06:00 +0200

    FileUpload refactor in progress

--- frontend/src/hooks/useApi.js
+++ frontend/src/hooks/useApi.js
@@ -4,94 +4,94 @@ import API from "../util/axiosConfig";
 import { AuthContext } from "../context/auth";
 
 function apiReducer(state, action) {
-  switch (action.type) {
-    case "INIT":
-      return { ...state, loading: true, error: false };
-    case "SUCCESS":
-      return { ...state, loading: false, error: false, data: action.payload };
-    case "FAILURE":
-      return { ...state, loading: false, error: true, data: action.payload };
-    case "HIDE_MSG":
-      return { ...state, data: null };
-    default:
-      return { ...state };
-  }
+	switch (action.type) {
+		case "INIT":
+			return { ...state, loading: true, error: false };
+		case "SUCCESS":
+			return { ...state, loading: false, error: false, data: action.payload };
+		case "FAILURE":
+			return { ...state, loading: false, error: true, data: action.payload };
+		case "HIDE_MSG":
+			return { ...state, data: null };
+		default:
+			return { ...state };
+	}
 }
 
 export function useDataFetch(initUrl, initData) {
-  const { logout } = useContext(AuthContext);
-  const [url, setUrl] = useState(initUrl);
-  const [refresh, setRefresh] = useState(false);
+	const { logout } = useContext(AuthContext);
+	const [url, setUrl] = useState(initUrl);
+	const [refresh, setRefresh] = useState(false);
 
-  const [state, dispatch] = useReducer(apiReducer, {
-    loading: false,
-    error: false,
-    data: initData,
-  });
+	const [state, dispatch] = useReducer(apiReducer, {
+		loading: true,
+		error: false,
+		data: initData,
+	});
 
-  useEffect(() => {
-    let cancel = false;
+	useEffect(() => {
+		let cancel = false;
 
-    async function fetchData() {
-      dispatch({ type: "INIT" });
+		async function fetchData() {
+			dispatch({ type: "INIT" });
 
-      try {
-        const res = await API.get(url);
-        if (!cancel) {
-          dispatch({ type: "SUCCESS", payload: res.data });
-        }
-      } catch (err) {
-        if (!cancel) {
-          if (err.response.status === 401) {
-            logout();
-          } else dispatch({ type: "FAILURE", payload: err.response.data });
-        }
-      }
-    }
+			try {
+				const res = await API.get(url);
+				if (!cancel) {
+					dispatch({ type: "SUCCESS", payload: res.data });
+				}
+			} catch (err) {
+				if (!cancel) {
+					if (err.response.status === 401) {
+						logout();
+					} else dispatch({ type: "FAILURE", payload: err.response.data });
+				}
+			}
+		}
 
-    fetchData();
-    return () => (cancel = true);
-  }, [url, refresh, logout]);
+		fetchData();
+		return () => (cancel = true);
+	}, [url, refresh, logout]);
 
-  function refreshData() {
-    setRefresh(!refresh);
-  }
+	function refreshData() {
+		setRefresh(!refresh);
+	}
 
-  return {
-    ...state,
-    setUrl,
-    refreshData,
-  };
+	return {
+		...state,
+		setUrl,
+		refreshData,
+	};
 }
 
 export function useDataSend() {
-  const { logout } = useContext(AuthContext);
+	const { logout } = useContext(AuthContext);
 
-  const [state, dispatch] = useReducer(apiReducer, {
-    loadig: true,
-    error: false,
-    data: null,
-  });
+	const [state, dispatch] = useReducer(apiReducer, {
+		loadig: true,
+		error: false,
+		data: null,
+	});
 
-  async function sendData(url, method, data, headers) {
-    dispatch({ type: "INIT" });
-    try {
-      const res = await API.request({ url, method, data, headers });
-      dispatch({ type: "SUCCESS", payload: res.data });
-      return res.data;
-    } catch (err) {
-      if (err.response.status === 401) {
-        logout();
-      } else {
-        dispatch({ type: "FAILURE", payload: err.response.data });
-        return err.response.data;
-      }
-    }
-  }
+	async function sendData(url, method, data, headers) {
+		dispatch({ type: "INIT" });
+		try {
+			const res = await API.request({ url, method, data, headers });
+			dispatch({ type: "SUCCESS", payload: res.data });
+			return res.data;
+		} catch (err) {
+			if (err.response.status === 401) {
+				logout();
+			} else {
+				dispatch({ type: "FAILURE", payload: err.response.data });
+				return err.response.data;
+			}
+		}
+	}
 
-  function hideMessage() {
-    dispatch({ type: "HIDE_MSG" });
-  }
+	function hideMessage() {
+		dispatch({ type: "HIDE_MSG" });
+	}
 
-  return { ...state, sendData, hideMessage };
+	return { ...state, sendData, hideMessage };
 }
