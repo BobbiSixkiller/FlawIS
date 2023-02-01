41658d0cfe3395b075772ba7ee4c56309f2f3e95
Matus Muransky <matus.muransky2@gmail.com>
2023-02-01 18:04:49 +0100

    mail service config change

--- client/src/components/UserVerifiedDialog.tsx
+++ client/src/components/UserVerifiedDialog.tsx
@@ -7,43 +7,43 @@ import { AuthContext } from "../providers/Auth";
 import { DialogContext } from "../providers/Dialog";
 
 export default function UserVerifiedDialog() {
-  const { t } = useTranslation("activation");
-  const [msg, setMsg] = useState("");
+	const { t } = useTranslation("activation");
+	const [msg, setMsg] = useState("");
 
-  const { pathname } = useRouter();
-  const { user } = useContext(AuthContext);
-  const { handleOpen } = useContext(DialogContext);
+	const { pathname } = useRouter();
+	const { user } = useContext(AuthContext);
+	const { handleOpen } = useContext(DialogContext);
 
-  const [resendActivationLink, { loading, error }] =
-    useResendActivationLinkMutation({
-      onCompleted: () => setMsg(t("activation.msg")),
-    });
+	const [resendActivationLink, { loading, error }] =
+		useResendActivationLinkMutation({
+			onCompleted: () => setMsg(t("dialog.msg")),
+		});
 
-  /* eslint-disable */
-  useEffect(() => {
-    const content = (
-      <>
-        <Header>{t("dialog.header")}</Header>
-        <p>{t("dialog.body")}</p>
-        <Button loading={loading} onClick={() => resendActivationLink()}>
-          {t("dialog.button")}{" "}
-        </Button>
-        {msg && (
-          <Message
-            positive
-            compact
-            content={msg}
-            onDismiss={() => setMsg("")}
-          />
-        )}
-        {error && <Message error compact content={error.message} />}
-      </>
-    );
+	/* eslint-disable */
+	useEffect(() => {
+		const content = (
+			<>
+				<Header>{t("dialog.header")}</Header>
+				<p>{t("dialog.body")}</p>
+				<Button loading={loading} onClick={() => resendActivationLink()}>
+					{t("dialog.button")}{" "}
+				</Button>
+				{msg && (
+					<Message
+						positive
+						compact
+						content={msg}
+						onDismiss={() => setMsg("")}
+					/>
+				)}
+				{error && <Message error compact content={error.message} />}
+			</>
+		);
 
-    if (user && !user?.verified && !pathname.includes("/activate")) {
-      handleOpen({ content, size: "tiny" });
-    }
-  }, [user, pathname, handleOpen, resendActivationLink, error, loading, msg]);
+		if (user && !user?.verified && !pathname.includes("/activate")) {
+			handleOpen({ content, size: "tiny" });
+		}
+	}, [user, pathname, handleOpen, resendActivationLink, error, loading, msg]);
 
-  return <div />;
+	return <div />;
 }
