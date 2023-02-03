b20f9152c30ffff5895e3a694ea9119d1ebcd009
Matus Muransky <matus.muransky2@gmail.com>
2023-02-03 15:20:25 +0100

    Users page implemented

--- users/api/resolvers/types/pagination.ts
+++ users/api/resolvers/types/pagination.ts
@@ -1,8 +1,10 @@
+import { Max, Min } from "class-validator";
 import { ObjectId } from "mongodb";
-import { ClassType, Field, ObjectType } from "type-graphql";
+import { Args, ArgsType, ClassType, Field, Int, ObjectType } from "type-graphql";
+import { RefDocExists } from "../../util/validation";
 
 //generic function for creating corresponding Connection Type enabling relay style pagination
-export default function CreateConnection<TNode>(TNodeClass: ClassType<TNode>) {
+export function CreateConnection<TNode>(TNodeClass: ClassType<TNode>) {
   // `isAbstract` decorator option is mandatory to prevent registering in schema
   @ObjectType(`${TNodeClass.name}Edge`)
   abstract class Edge {
@@ -32,4 +34,23 @@ export default function CreateConnection<TNode>(TNodeClass: ClassType<TNode>) {
   }
 
   return Connection;
+}
+
+
+export function CreateArgs<TNode>(TNodeClass: ClassType<TNode>) {
+  @ArgsType()
+  class ConnectionArgs {
+    @Field(() => ObjectId, { nullable: true })
+    @RefDocExists(TNodeClass, {
+      message: "Cursor's document not found!",
+    })
+    after?: ObjectId;
+
+    @Field(() => Int, { defaultValue: 20 })
+    @Min(1)
+    @Max(50)
+    first: number = 20;
+  }
+
+  return ConnectionArgs
 }
