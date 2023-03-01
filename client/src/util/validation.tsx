6d6c6e30c831e610137860e7619c7e55e81528a5
Matus Muransky <matus.muransky2@gmail.com>
2023-03-01 14:30:00 +0100

    string trimming in validation implemented

--- client/src/util/validation.tsx
+++ client/src/util/validation.tsx
@@ -2,82 +2,86 @@ import { useTranslation } from "next-i18next";
 import { object, ref, setLocale, string } from "yup";
 
 export function checkIfFilesAreTooBig(file: File): boolean {
-	console.log(file.size, 10000000);
-	return file.size < 10000000; //1MB
+  console.log(file.size, 10000000);
+  return file.size < 10000000; //1MB
 }
 
 export function checkIfFilesAreCorrectType(file: File): boolean {
-	return [
-		"application/pdf",
-		"image/jpeg",
-		"image/png",
-		"application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-	].includes(file.type);
+  return [
+    "application/pdf",
+    "image/jpeg",
+    "image/png",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+  ].includes(file.type);
 }
 
 export default function Validation() {
-	const { t } = useTranslation("validation");
+  const { t } = useTranslation("validation");
 
-	setLocale({
-		mixed: {
-			required: t("required"),
-		},
-		string: {
-			email: t("email"),
-		},
-	});
+  setLocale({
+    mixed: {
+      required: t("required"),
+    },
+    string: {
+      email: t("email"),
+    },
+  });
 
-	const loginInputSchema = object({
-		email: string().email().required(),
-		password: string().required(),
-	});
+  const loginInputSchema = object({
+    email: string().email().required(),
+    password: string().trim().required(),
+  });
 
-	const registerInputSchema = object({
-		name: string().required(),
-		email: string()
-			.required()
-			.email()
-			.test(
-				"isUniba",
-				t("unibaEmail"),
-				(val) =>
-					val !== undefined &&
-					val?.includes("@") &&
-					val?.split("@")[1].includes("uniba")
-			),
-		password: string()
-			.required()
-			.matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, t("password")),
-		repeatPass: string()
-			.required()
-			.oneOf([ref("password")], t("passNoMatch")),
-	});
+  const registerInputSchema = object({
+    name: string().trim().required(),
+    email: string()
+      .required()
+      .email()
+      .test(
+        "isUniba",
+        t("unibaEmail"),
+        (val) =>
+          val !== undefined &&
+          val?.includes("@") &&
+          val?.split("@")[1].includes("uniba")
+      ),
+    password: string()
+      .trim()
+      .required()
+      .matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, t("password")),
+    repeatPass: string()
+      .trim()
+      .required()
+      .oneOf([ref("password")], t("passNoMatch")),
+  });
 
-	const forgotPasswordInputSchema = object({
-		email: string().required().email(),
-	});
+  const forgotPasswordInputSchema = object({
+    email: string().required().email(),
+  });
 
-	const passwordInputSchema = object({
-		password: string()
-			.required()
-			.matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, t("password")),
-		repeatPass: string()
-			.required()
-			.oneOf([ref("password")], t("passNoMatch")),
-	});
+  const passwordInputSchema = object({
+    password: string()
+      .trim()
+      .required()
+      .matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, t("password")),
+    repeatPass: string()
+      .trim()
+      .required()
+      .oneOf([ref("password")], t("passNoMatch")),
+  });
 
-	const perosnalInfoInputSchema = object({
-		name: string().required(),
-		email: string().required().email(),
-		organisation: string().required(),
-		telephone: string().required(),
-	});
+  const perosnalInfoInputSchema = object({
+    name: string().trim().required(),
+    email: string().required().email(),
+    organisation: string().trim().required(),
+    telephone: string().trim().required(),
+  });
 
-	return {
-		loginInputSchema,
-		registerInputSchema,
-		forgotPasswordInputSchema,
-		passwordInputSchema,
-		perosnalInfoInputSchema,
-	};
+  return {
+    loginInputSchema,
+    registerInputSchema,
+    forgotPasswordInputSchema,
+    passwordInputSchema,
+    perosnalInfoInputSchema,
+  };
 }
