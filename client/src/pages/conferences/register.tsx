8938ec229c786260d329dc7a19ab8ad6c8eb2a06
Matus Muransky <matus.muransky2@gmail.com>
2023-03-02 10:57:58 +0100

    Register form for conferences refactored

--- client/src/pages/conferences/register.tsx
+++ client/src/pages/conferences/register.tsx
@@ -7,238 +7,247 @@ import logo from "public/images/Flaw-logo-notext.png";
 import { FC, useContext, useEffect } from "react";
 
 import {
-	Button,
-	Form,
-	Grid,
-	Header,
-	Input,
-	Message,
-	Segment,
+  Button,
+  Form,
+  Grid,
+  Header,
+  Input,
+  Message,
+  Segment,
 } from "semantic-ui-react";
 
 import { boolean, InferType, object, ref, string } from "yup";
 import CheckboxField from "../../components/form/CheckboxField";
 import { InputField, InputFieldProps } from "../../components/form/InputField";
 import {
-	useRegisterMutation,
-	useUpdateConferenceUserMutation,
+  useRegisterMutation,
+  useUpdateConferenceUserMutation,
 } from "../../graphql/generated/schema";
 import { ActionTypes, AuthContext } from "../../providers/Auth";
 import parseErrors from "../../util/parseErrors";
+import Validation from "../../util/validation";
 
 const registerInputSchema = object({
-	name: string().required(),
-	email: string().required().email(),
-	organisation: string().required(),
-	telephone: string().required(),
-	password: string().required(),
-	repeatPass: string()
-		.required()
-		.oneOf([ref("password")]),
-	terms: boolean()
-		.oneOf([true], "You must agree with the Privacy policy")
-		.required(),
+  name: string().required(),
+  email: string().required().email(),
+  organisation: string().required(),
+  telephone: string().required(),
+  password: string().required(),
+  repeatPass: string()
+    .required()
+    .oneOf([ref("password")]),
+  terms: boolean()
+    .oneOf([true], "You must agree with the Privacy policy")
+    .required(),
 });
 
-type Values = InferType<typeof registerInputSchema>;
-
 const EmailField: FC<InputFieldProps> = (props) => {
-	const { values, setFieldValue, errors, touched } = useFormikContext<Values>();
+  const { perosnalInfoInputSchema } = Validation();
+  type Values = InferType<typeof perosnalInfoInputSchema>;
 
-	useEffect(() => {
-		if (
-			touched["email"] &&
-			!errors["email"] &&
-			values["email"].split("@")[1] === "flaw.uniba.sk"
-		) {
-			setFieldValue(
-				"organisation",
-				"Univerzita Komenského v Bratislave, Právnická fakulta"
-			);
-		}
-	}, [values, setFieldValue, errors, touched]);
+  const { values, setFieldValue, errors, touched } = useFormikContext<Values>();
 
-	return <InputField {...props} />;
+  useEffect(() => {
+    if (
+      touched["email"] &&
+      !errors["email"] &&
+      values["email"].split("@")[1] === "flaw.uniba.sk"
+    ) {
+      setFieldValue(
+        "organisation",
+        "Univerzita Komenského v Bratislave, Právnická fakulta"
+      );
+    }
+  }, [values, setFieldValue, errors, touched]);
+
+  return <InputField {...props} />;
 };
 
 const Register: NextPage = () => {
-	const { dispatch } = useContext(AuthContext);
-	const router = useRouter();
+  const { dispatch } = useContext(AuthContext);
+  const router = useRouter();
 
-	const [register] = useRegisterMutation({
-		onCompleted: ({ register }) => {
-			dispatch({ type: ActionTypes.Login, payload: { user: register } });
-		},
-	});
+  const { perosnalInfoInputSchema, passwordInputSchema, termsInputSchmema } =
+    Validation();
+  const registerInputSchema = perosnalInfoInputSchema
+    .concat(passwordInputSchema)
+    .concat(termsInputSchmema);
+  type Values = InferType<typeof registerInputSchema>;
 
-	const [updateConferenceUser] = useUpdateConferenceUserMutation();
+  const [register] = useRegisterMutation({
+    onCompleted: ({ register }) => {
+      dispatch({ type: ActionTypes.Login, payload: { user: register } });
+    },
+  });
 
-	return (
-		<Grid container centered>
-			<Grid.Row>
-				<Grid.Column style={{ maxWidth: 340 }}>
-					<div
-						style={{
-							width: "100%",
-							display: "flex",
-							flexDirection: "row",
-							justifyContent: "center",
-							paddingTop: "32px",
-							cursor: "pointer",
-						}}
-					>
-						<Link href="/">
-							<Image
-								alt="flaw-logo-notext"
-								src={logo}
-								height={48}
-								width={48}
-								priority={true}
-							/>
-						</Link>
-					</div>
+  const [updateConferenceUser] = useUpdateConferenceUserMutation();
 
-					<Header as="h2" textAlign="center">
-						Register
-					</Header>
-					<Formik
-						initialValues={{
-							name: "",
-							email: "",
-							organisation: "",
-							telephone: "",
-							password: "",
-							repeatPass: "",
-							terms: false,
-						}}
-						validationSchema={registerInputSchema}
-						onSubmit={async (values, actions) => {
-							try {
-								await register({
-									variables: {
-										data: {
-											email: values.email,
-											name: values.name,
-											password: values.password,
-										},
-									},
-								});
-								await updateConferenceUser({
-									variables: {
-										data: {
-											organisation: values.organisation,
-											telephone: values.telephone,
-										},
-									},
-								});
-								router.push("/");
-							} catch (err: any) {
-								actions.setStatus(
-									parseErrors(
-										err.graphQLErrors[0].extensions.exception.validationErrors
-									)
-								);
-							}
-						}}
-					>
-						{({ handleSubmit, isSubmitting }: FormikProps<Values>) => (
-							<Form size="large" autoComplete="off" onSubmit={handleSubmit}>
-								<Segment>
-									<InputField
-										fluid
-										icon="user"
-										iconPosition="left"
-										placeholder="Name including titles"
-										label="Name including titles"
-										name="name"
-										control={Input}
-									/>
+  return (
+    <Grid container centered>
+      <Grid.Row>
+        <Grid.Column style={{ maxWidth: 340 }}>
+          <div
+            style={{
+              width: "100%",
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              paddingTop: "32px",
+              cursor: "pointer",
+            }}
+          >
+            <Link href="/">
+              <Image
+                alt="flaw-logo-notext"
+                src={logo}
+                height={48}
+                width={48}
+                priority={true}
+              />
+            </Link>
+          </div>
 
-									<EmailField
-										fluid
-										icon="at"
-										iconPosition="left"
-										placeholder="E-mail address"
-										label="Email"
-										name="email"
-										control={Input}
-									/>
+          <Header as="h2" textAlign="center">
+            Register
+          </Header>
+          <Formik
+            initialValues={{
+              name: "",
+              email: "",
+              organisation: "",
+              telephone: "",
+              password: "",
+              repeatPass: "",
+              terms: false,
+            }}
+            validationSchema={registerInputSchema}
+            onSubmit={async (values, actions) => {
+              try {
+                await register({
+                  variables: {
+                    data: {
+                      email: values.email,
+                      name: values.name,
+                      password: values.password,
+                    },
+                  },
+                });
+                await updateConferenceUser({
+                  variables: {
+                    data: {
+                      organisation: values.organisation,
+                      telephone: values.telephone,
+                    },
+                  },
+                });
+                router.push("/");
+              } catch (err: any) {
+                actions.setStatus(
+                  parseErrors(
+                    err.graphQLErrors[0].extensions.exception.validationErrors
+                  )
+                );
+              }
+            }}
+          >
+            {({ handleSubmit, isSubmitting }: FormikProps<Values>) => (
+              <Form size="large" autoComplete="off" onSubmit={handleSubmit}>
+                <Segment>
+                  <InputField
+                    fluid
+                    icon="user"
+                    iconPosition="left"
+                    placeholder="Name including titles"
+                    label="Name including titles"
+                    name="name"
+                    control={Input}
+                  />
 
-									<InputField
-										fluid
-										icon="phone"
-										iconPosition="left"
-										placeholder="Telephone number"
-										label="Telephone"
-										name="telephone"
-										control={Input}
-									/>
+                  <EmailField
+                    fluid
+                    icon="at"
+                    iconPosition="left"
+                    placeholder="E-mail address"
+                    label="Email"
+                    name="email"
+                    control={Input}
+                  />
 
-									<InputField
-										fluid
-										icon="building"
-										iconPosition="left"
-										placeholder="Name of the organisation"
-										label="Organisation"
-										name="organisation"
-										control={Input}
-									/>
+                  <InputField
+                    fluid
+                    icon="phone"
+                    iconPosition="left"
+                    placeholder="Telephone number"
+                    label="Telephone"
+                    name="telephone"
+                    control={Input}
+                  />
 
-									<InputField
-										fluid
-										icon="lock"
-										iconPosition="left"
-										placeholder="Password"
-										type="password"
-										label="Password"
-										name="password"
-										control={Input}
-									/>
+                  <InputField
+                    fluid
+                    icon="building"
+                    iconPosition="left"
+                    placeholder="Name of the organisation"
+                    label="Organisation"
+                    name="organisation"
+                    control={Input}
+                  />
 
-									<InputField
-										fluid
-										icon="lock"
-										iconPosition="left"
-										placeholder="Password"
-										type="password"
-										label="Repeat Password"
-										name="repeatPass"
-										control={Input}
-									/>
+                  <InputField
+                    fluid
+                    icon="lock"
+                    iconPosition="left"
+                    placeholder="Password"
+                    type="password"
+                    label="Password"
+                    name="password"
+                    control={Input}
+                  />
 
-									<CheckboxField
-										name="terms"
-										label={
-											<label>
-												I agree with the{" "}
-												<Link href="https://uniba.sk/en/privacy-policy/">
-													Privacy policy
-												</Link>
-											</label>
-										}
-									/>
+                  <InputField
+                    fluid
+                    icon="lock"
+                    iconPosition="left"
+                    placeholder="Password"
+                    type="password"
+                    label="Repeat Password"
+                    name="repeatPass"
+                    control={Input}
+                  />
 
-									<Button
-										type="submit"
-										loading={isSubmitting}
-										disabled={isSubmitting}
-										fluid
-										size="large"
-									>
-										Register
-									</Button>
-								</Segment>
-							</Form>
-						)}
-					</Formik>
+                  <CheckboxField
+                    name="terms"
+                    label={
+                      <label>
+                        I agree with the{" "}
+                        <Link href="https://uniba.sk/en/privacy-policy/">
+                          Privacy policy
+                        </Link>
+                      </label>
+                    }
+                  />
 
-					<Message style={{ textAlign: "center" }}>
-						Already have an account? <Link href="/login">Log In!</Link>
-					</Message>
-				</Grid.Column>
-			</Grid.Row>
-		</Grid>
-	);
+                  <Button
+                    type="submit"
+                    loading={isSubmitting}
+                    disabled={isSubmitting}
+                    fluid
+                    size="large"
+                  >
+                    Register
+                  </Button>
+                </Segment>
+              </Form>
+            )}
+          </Formik>
+
+          <Message style={{ textAlign: "center" }}>
+            Already have an account? <Link href="/login">Log In!</Link>
+          </Message>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  );
 };
 
 export default Register;
