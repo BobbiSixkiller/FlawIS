86a7deca3ad9f8e3394fbf59e6155640427750b0
Matus Muransky <matus.muransky2@gmail.com>
2023-03-14 09:49:30 +0100

    Fileupload implemeneted

--- client/src/components/form/Upload/SingleUploadProgress.tsx
+++ client/src/components/form/Upload/SingleUploadProgress.tsx
@@ -2,112 +2,115 @@ import { useEffect, useState } from "react";
 import { FileError } from "react-dropzone";
 import { Button, Progress } from "semantic-ui-react";
 import styled from "styled-components";
-import {
-  UploadFileMutationResult,
-  useDeleteFileMutation,
-} from "../../../graphql/generated/schema";
+import { useDeleteFileMutation } from "../../../graphql/generated/schema";
 
 const UploadIndicator = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 8px;
-  gap: 4px;
+	display: flex;
+	flex-direction: column;
+	padding: 8px;
+	gap: 4px;
 `;
 
 const LabelContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: flex-end;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: flex-end;
 `;
 
 export interface SingleUploadProgressProps {
-  file: File;
-  errors: FileError[];
-  url?: string;
-  onDelete: (file: File) => void;
-  onUpload: (file: File, url: string) => void;
+	file: File;
+	errors: FileError[];
+	url?: string;
+	onDelete: (file: File) => void;
+	onUpload: (file: File, url: string, errors: FileError[]) => void;
 }
 
 export default function SingleUploadProgress({
-  file,
-  errors,
-  url,
-  onDelete,
-  onUpload,
+	file,
+	errors,
+	url,
+	onDelete,
+	onUpload,
 }: SingleUploadProgressProps) {
-  const [progress, setProgress] = useState(0);
-  const [deleteFile, { loading }] = useDeleteFileMutation();
+	const [progress, setProgress] = useState(0);
+	const [deleteFile, { loading }] = useDeleteFileMutation();
 
-  useEffect(() => {
-    async function upload() {
-      const url = await uploadFile(file, setProgress);
-      onUpload(file, url);
-    }
+	useEffect(() => {
+		async function upload() {
+			const res = await uploadFile(file, setProgress);
+			onUpload(file, res.data?.uploadFile, res.errors || []);
+		}
 
-    if (errors.length === 0) {
-      upload();
-    }
-  }, []);
+		if (errors.length === 0) {
+			upload();
+		}
+	}, []);
 
-  return (
-    <UploadIndicator>
-      <LabelContainer>
-        {file.name}
-        <Button
-          type="button"
-          icon="trash"
-          size="mini"
-          loading={loading}
-          onClick={async () => {
-            if (url) {
-              await deleteFile({ variables: { url } });
-            }
-            onDelete(file);
-          }}
-        />
-      </LabelContainer>
-      <Progress
-        percent={progress}
-        size="tiny"
-        error={errors.length !== 0}
-        autoSuccess
-      />
-    </UploadIndicator>
-  );
+	return (
+		<UploadIndicator>
+			<LabelContainer>
+				{file.name}
+				<Button
+					type="button"
+					icon="trash"
+					size="mini"
+					loading={loading}
+					onClick={async () => {
+						if (url) {
+							await deleteFile({ variables: { url } });
+						}
+						onDelete(file);
+					}}
+				/>
+			</LabelContainer>
+			<Progress
+				percent={progress}
+				size="tiny"
+				error={errors.length !== 0}
+				autoSuccess
+			>
+				{errors.map((e) => e.message)}
+			</Progress>
+		</UploadIndicator>
+	);
 }
 
 function uploadFile(file: File, onProgress: (percentage: number) => void) {
-  const url = "http://localhost:5000/graphql";
+	const url = "http://localhost:5000/graphql";
 
-  return new Promise<string>((res, rej) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", url);
+	return new Promise<{
+		errors?: FileError[];
+		data?: any;
+	}>((res, rej) => {
+		const xhr = new XMLHttpRequest();
+		xhr.open("POST", url);
 
-    xhr.onload = () => {
-      const response: UploadFileMutationResult = JSON.parse(xhr.responseText);
-      res(response.data?.uploadFile || "");
-    };
-    xhr.onerror = (evt) => rej(evt);
-    xhr.upload.onprogress = (event) => {
-      if (event.lengthComputable) {
-        const percentage = (event.loaded / event.total) * 100;
-        onProgress(Math.round(percentage));
-      }
-    };
+		xhr.onload = () => {
+			const response = JSON.parse(xhr.responseText);
+			console.log(response);
+			res(response);
+		};
+		xhr.onerror = (evt) => rej(evt);
+		xhr.upload.onprogress = (event) => {
+			if (event.lengthComputable) {
+				const percentage = (event.loaded / event.total) * 100;
+				onProgress(Math.round(percentage));
+			}
+		};
 
-    const formData = new FormData();
-    formData.append(
-      "operations",
-      JSON.stringify({
-        query:
-          "mutation uploadFile($file:Upload!) {\n  uploadFile(file: $file, type: GRANT)\n}",
-      })
-    );
-    formData.append("map", JSON.stringify({ "0": ["variables.file"] }));
-    formData.append("0", file);
+		const formData = new FormData();
+		formData.append(
+			"operations",
+			JSON.stringify({
+				query:
+					"mutation uploadFile($file:Upload!) {\n  uploadFile(file: $file, type: GRANT)\n}",
+			})
+		);
+		formData.append("map", JSON.stringify({ "0": ["variables.file"] }));
+		formData.append("0", file);
 
-    xhr.withCredentials = true;
-    xhr.send(formData);
-  });
+		xhr.withCredentials = true;
+		xhr.send(formData);
+	});
 }
