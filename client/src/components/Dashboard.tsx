fcb620d127830317147bfe87609b690ae62dca65
Matus Muransky <matus.muransky2@gmail.com>
2023-04-18 12:48:54 +0200

    sticky container set to fixed

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,13 +1,5 @@
 import { ReactNode, useContext } from "react";
-import {
-	Button,
-	Divider,
-	Dropdown,
-	Grid,
-	Image,
-	List,
-	Menu,
-} from "semantic-ui-react";
+import { Divider, Grid, List, Menu } from "semantic-ui-react";
 
 import useWith from "../hooks/useWidth";
 import Nav, { ContentWrapper } from "./MobileNav";
@@ -21,87 +13,87 @@ import Link from "next/link";
 import styled from "styled-components";
 
 const StickyContainer = styled.div`
-	position: sticky;
-	top: 16;
-	display: flex;
-	flex-direction: column;
-	justify-content: space-between;
-	height: calc(100vh - 48px);
-	& div a {
-		display: flex;
-		justify-content: center;
-	}
+  position: fixed;
+  top: 16;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: calc(100vh - 48px);
+  & div a {
+    display: flex;
+    justify-content: center;
+  }
 `;
 
 interface dashboardProps {
-	children: ReactNode;
+  children: ReactNode;
 }
 
 export default function Dashboard({ children }: dashboardProps) {
-	const { rightPanelItems } = useContext(ControlsContext);
-	const width = useWith();
+  const { rightPanelItems } = useContext(ControlsContext);
+  const width = useWith();
 
-	const { t } = useTranslation("common");
+  const { t } = useTranslation("common");
 
-	if (width < 768) {
-		return (
-			<Nav transparent={false} locales={true}>
-				<ContentWrapper>
-					<Grid container>
-						<Grid.Row>
-							<Grid.Column>
-								{rightPanelItems && (
-									<div style={{ paddingTop: "1rem", paddingBottom: "1rem" }}>
-										{rightPanelItems}
-										<Divider />
-									</div>
-								)}
-								{children}
-							</Grid.Column>
-						</Grid.Row>
-					</Grid>
-				</ContentWrapper>
-				<Footer />
-			</Nav>
-		);
-	}
+  if (width < 768) {
+    return (
+      <Nav transparent={false} locales={true}>
+        <ContentWrapper>
+          <Grid container>
+            <Grid.Row>
+              <Grid.Column>
+                {rightPanelItems && (
+                  <div style={{ paddingTop: "1rem", paddingBottom: "1rem" }}>
+                    {rightPanelItems}
+                    <Divider />
+                  </div>
+                )}
+                {children}
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </ContentWrapper>
+        <Footer />
+      </Nav>
+    );
+  }
 
-	return (
-		<Grid
-			stackable
-			container
-			divided
-			style={{ marginTop: 0, marginBottom: 0, minHeight: "100vh" }}
-		>
-			<Grid.Column width={3} only="tablet computer">
-				<StickyContainer>
-					<Menu text vertical compact>
-						<Menu.Item as={Link} href="/">
-							<NextImage
-								alt="flaw-logo-notext"
-								src={logo}
-								height={35}
-								width={35}
-							/>
-						</Menu.Item>
-						<MainMenuItems />
-					</Menu>
-				</StickyContainer>
-			</Grid.Column>
-			<Grid.Column width={9}>{children}</Grid.Column>
-			<Grid.Column width={4} only="tablet computer">
-				<StickyContainer>
-					<div>{rightPanelItems}</div>
-					<List link size="small">
-						<List.Item as="a" href="#">
-							{t("footer.menu.contact")}
-						</List.Item>
-						<List.Item as="a" href="#">
-							{t("footer.menu.terms")}
-						</List.Item>
-					</List>
-				</StickyContainer>
-			</Grid.Column>
-		</Grid>
-	);
+  return (
+    <Grid
+      stackable
+      container
+      divided
+      style={{ marginTop: 0, marginBottom: 0, minHeight: "100vh" }}
+    >
+      <Grid.Column width={3} only="tablet computer">
+        <StickyContainer>
+          <Menu text vertical compact>
+            <Menu.Item as={Link} href="/">
+              <NextImage
+                alt="flaw-logo-notext"
+                src={logo}
+                height={35}
+                width={35}
+              />
+            </Menu.Item>
+            <MainMenuItems />
+          </Menu>
+        </StickyContainer>
+      </Grid.Column>
+      <Grid.Column width={9}>{children}</Grid.Column>
+      <Grid.Column width={4} only="tablet computer">
+        <StickyContainer>
+          <div>{rightPanelItems}</div>
+          <List link size="small">
+            <List.Item as="a" href="#">
+              {t("footer.menu.contact")}
+            </List.Item>
+            <List.Item as="a" href="#">
+              {t("footer.menu.terms")}
+            </List.Item>
+          </List>
+        </StickyContainer>
+      </Grid.Column>
+    </Grid>
+  );
 }
