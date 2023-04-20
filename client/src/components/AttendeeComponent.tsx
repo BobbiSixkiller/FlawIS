703f9cabcbf29bf684591beac74eb041cc1595d4
BobbiSixkiller <matus.muransky2@gmail.com>
2023-04-20 21:25:48 +0200

    no clue

--- client/src/components/AttendeeComponent.tsx
+++ client/src/components/AttendeeComponent.tsx
@@ -1,10 +1,10 @@
 import { Card, Grid, Header, List, Loader, Message } from "semantic-ui-react";
 import { useTranslation } from "next-i18next";
 import {
-  AttendeeFragmentFragment,
-  Invoice,
-  Role,
-  useDeleteSubmissionMutation,
+	AttendeeFragmentFragment,
+	Invoice,
+	Role,
+	useDeleteSubmissionMutation,
 } from "../graphql/generated/schema";
 import useWidth from "../hooks/useWidth";
 import DeleteDialog from "./DeleteDialog";
@@ -17,144 +17,128 @@ import { AuthContext } from "../providers/Auth";
 import UpdateInvoiceForm from "./UpdateInvoiceForm";
 
 export default function AttendeeComponent({
-  title,
-  data,
-  error,
-  loading,
+	title,
+	data,
 }: {
-  title?: string;
-  data?: AttendeeFragmentFragment | null;
-  error?: any;
-  loading: boolean;
+	title?: string;
+	data?: AttendeeFragmentFragment | null;
 }) {
-  const { t } = useTranslation("conference");
-  const { user } = useContext(AuthContext);
-  const width = useWidth();
+	const { t } = useTranslation("conference");
+	const { user } = useContext(AuthContext);
+	const width = useWidth();
 
-  const [deleteSubmission] = useDeleteSubmissionMutation();
+	const [deleteSubmission] = useDeleteSubmissionMutation();
 
-  if (loading) {
-    return (
-      <Grid.Column style={{ height: "400px" }}>
-        <Loader active />
-      </Grid.Column>
-    );
-  }
-
-  if (error) {
-    return <Message negative content={error.message} />;
-  }
-
-  return (
-    <Grid padded={width < 400 ? "vertically" : true}>
-      <Grid.Row>
-        <Grid.Column>
-          <Header>{title}</Header>
-        </Grid.Column>
-        {data?.submissions.length !== 0 && (
-          <Grid.Column>
-            <AddSubmissionDialog />
-          </Grid.Column>
-        )}
-      </Grid.Row>
-      <Grid.Row>
-        <Grid.Column>
-          {user?.role === Role.Admin ? (
-            <UpdateInvoiceForm
-              data={data?.invoice}
-              downloadLink={
-                <InvoiceDownload
-                  data={data?.invoice as Invoice}
-                  conferenceLogo={data?.conference.logoUrl as string}
-                />
-              }
-            />
-          ) : (
-            <InvoiceDownload
-              data={data?.invoice as Invoice}
-              conferenceLogo={data?.conference.logoUrl as string}
-            />
-          )}
-        </Grid.Column>
-      </Grid.Row>
-      {data?.submissions?.length !== 0 && (
-        <Grid.Row>
-          <Grid.Column>
-            <Header>{t("dashboard.attendee.submission")}</Header>
-            {data?.submissions?.map((submission) => (
-              <Card fluid key={submission.id}>
-                <Card.Content>
-                  <Card.Header> {submission.name}</Card.Header>
-                  <Card.Meta>{submission.section.name}</Card.Meta>
-                  <Card.Description>
-                    <List bulleted horizontal>
-                      {submission.authors.map((a) => (
-                        <List.Item key={a.id}>{a.name}</List.Item>
-                      ))}
-                    </List>
-                    <p>{submission.abstract}</p>
-                    <List bulleted horizontal>
-                      {submission.keywords.map((k, i) => (
-                        <List.Item key={i}>{k}</List.Item>
-                      ))}
-                    </List>
-                  </Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                  <DeleteDialog
-                    confirmCb={async () =>
-                      (await deleteSubmission({
-                        variables: { id: submission.id },
-                        update(cache) {
-                          cache.evict({ id: `Submission:${submission.id}` });
-                        },
-                      })) as Promise<void>
-                    }
-                    header="Zmazať príspevok"
-                    content={<p>Naozaj chcete zmazať vybraný príspevok?</p>}
-                    cancelText="Zrušiť"
-                    confirmText="Potvrdiť"
-                  />
-                  <UpdateSubmissionDialog
-                    id={submission.id}
-                    input={{
-                      conferenceId: data.conference.id,
-                      authors: [],
-                      abstract: submission.abstract,
-                      keywords: submission.keywords,
-                      name: submission.name,
-                      sectionId: submission.section.id,
-                      translations: submission.translations.map((t) => ({
-                        language: t.language,
-                        name: t.name,
-                        abstract: t.abstract,
-                        keywords: t.keywords,
-                      })),
-                    }}
-                  />
-                  <AddSubmissionFileDialog
-                    id={submission.id}
-                    input={{
-                      conferenceId: data.conference.id,
-                      authors: [],
-                      abstract: submission.abstract,
-                      keywords: submission.keywords,
-                      name: submission.name,
-                      sectionId: submission.section.id,
-                      translations: submission.translations.map((t) => ({
-                        language: t.language,
-                        name: t.name,
-                        abstract: t.abstract,
-                        keywords: t.keywords,
-                      })),
-                    }}
-                  />
-                </Card.Content>
-              </Card>
-            ))}
-          </Grid.Column>
-        </Grid.Row>
-      )}
-    </Grid>
-  );
+	return (
+		<Grid padded={width < 400 ? "vertically" : true}>
+			<Grid.Row>
+				<Grid.Column>
+					<Header>{title}</Header>
+				</Grid.Column>
+				{data?.submissions.length !== 0 && (
+					<Grid.Column>
+						<AddSubmissionDialog />
+					</Grid.Column>
+				)}
+			</Grid.Row>
+			<Grid.Row>
+				<Grid.Column>
+					{user?.role === Role.Admin ? (
+						<UpdateInvoiceForm
+							data={data?.invoice}
+							downloadLink={
+								<InvoiceDownload
+									data={data?.invoice as Invoice}
+									conferenceLogo={data?.conference.logoUrl as string}
+								/>
+							}
+						/>
+					) : (
+						<InvoiceDownload
+							data={data?.invoice as Invoice}
+							conferenceLogo={data?.conference.logoUrl as string}
+						/>
+					)}
+				</Grid.Column>
+			</Grid.Row>
+			{data?.submissions?.length !== 0 && (
+				<Grid.Row>
+					<Grid.Column>
+						<Header>{t("dashboard.attendee.submission")}</Header>
+						{data?.submissions?.map((submission) => (
+							<Card fluid key={submission.id}>
+								<Card.Content>
+									<Card.Header> {submission.name}</Card.Header>
+									<Card.Meta>{submission.section.name}</Card.Meta>
+									<Card.Description>
+										<List bulleted horizontal>
+											{submission.authors.map((a) => (
+												<List.Item key={a.id}>{a.name}</List.Item>
+											))}
+										</List>
+										<p>{submission.abstract}</p>
+										<List bulleted horizontal>
+											{submission.keywords.map((k, i) => (
+												<List.Item key={i}>{k}</List.Item>
+											))}
+										</List>
+									</Card.Description>
+								</Card.Content>
+								<Card.Content extra>
+									<DeleteDialog
+										confirmCb={async () =>
+											(await deleteSubmission({
+												variables: { id: submission.id },
+												update(cache) {
+													cache.evict({ id: `Submission:${submission.id}` });
+												},
+											})) as Promise<void>
+										}
+										header="Zmazať príspevok"
+										content={<p>Naozaj chcete zmazať vybraný príspevok?</p>}
+										cancelText="Zrušiť"
+										confirmText="Potvrdiť"
+									/>
+									<UpdateSubmissionDialog
+										id={submission.id}
+										input={{
+											conferenceId: data.conference.id,
+											authors: [],
+											abstract: submission.abstract,
+											keywords: submission.keywords,
+											name: submission.name,
+											sectionId: submission.section.id,
+											translations: submission.translations.map((t) => ({
+												language: t.language,
+												name: t.name,
+												abstract: t.abstract,
+												keywords: t.keywords,
+											})),
+										}}
+									/>
+									<AddSubmissionFileDialog
+										id={submission.id}
+										input={{
+											conferenceId: data.conference.id,
+											authors: [],
+											abstract: submission.abstract,
+											keywords: submission.keywords,
+											name: submission.name,
+											sectionId: submission.section.id,
+											translations: submission.translations.map((t) => ({
+												language: t.language,
+												name: t.name,
+												abstract: t.abstract,
+												keywords: t.keywords,
+											})),
+										}}
+									/>
+								</Card.Content>
+							</Card>
+						))}
+					</Grid.Column>
+				</Grid.Row>
+			)}
+		</Grid>
+	);
 }
