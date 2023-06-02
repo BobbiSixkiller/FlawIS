de027b620998a615a713ab5e3c66de84dfb79859
Matus Muransky <matus.muransky2@gmail.com>
2023-06-02 13:08:24 +0200

    testing

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -1,36 +1,13 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
-import { ServerStyleSheet } from "styled-components";
+import { Html, Head, Main, NextScript } from "next/document";
 
-export default class MyDocument extends Document {
-  static async getInitialProps(ctx: any) {
-    const { renderPage } = ctx;
-    const initialProps = await Document.getInitialProps(ctx);
-
-    // Step 1: Create an instance of ServerStyleSheet
-    const sheet = new ServerStyleSheet();
-
-    // Step 2: Retrieve styles from components in the page
-    const page = renderPage(
-      (App: any) => (props: any) => sheet.collectStyles(<App {...props} />)
-    );
-
-    // Step 3: Extract the styles as <style> tags
-    const styleTags = sheet.getStyleElement();
-
-    // Step 4: Pass styleTags as a prop
-    return { ...initialProps, ...page, styleTags };
-  }
-
-  render() {
-    const { styleTags } = this.props as any;
-    return (
-      <Html>
-        <Head>{styleTags}</Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html lang="sk">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
