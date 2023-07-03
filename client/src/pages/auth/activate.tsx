9c7382102d57dbc266cb20064c4add8c1614a686
Matus Muransky <matus.muransky2@gmail.com>
2023-07-03 09:51:10 +0200

    account activation fixed

--- client/src/pages/auth/activate.tsx
+++ client/src/pages/auth/activate.tsx
@@ -2,7 +2,7 @@ import { NextPage } from "next";
 import { useTranslation } from "next-i18next";
 import Image from "next/image";
 import NextLink from "next/link";
-import { Grid, Header, Loader } from "semantic-ui-react";
+import { Button, Grid, Header, Loader } from "semantic-ui-react";
 import logo from "public/images/Flaw-logo-notext.png";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useActivateUserMutation } from "../../graphql/generated/schema";
@@ -11,79 +11,86 @@ import { useContext, useEffect, useRef } from "react";
 import { AuthContext } from "../../providers/Auth";
 
 const ActivateUserAccount: NextPage = () => {
-	const { query, push } = useRouter();
-	const { t } = useTranslation("activation");
-	const mount = useRef(false);
-	const { user } = useContext(AuthContext);
-	console.log(user);
+  const { query, push } = useRouter();
+  const { t } = useTranslation("activation");
+  const mount = useRef(false);
+  const { user } = useContext(AuthContext);
 
-	const [activateAccount, { loading }] = useActivateUserMutation({
-		update(cache) {
-			cache.modify({
-				id: `User:${user?.id}`,
-				fields: {
-					verified() {
-						return true;
-					},
-				},
-			});
-		},
-		onCompleted: () => push("/"),
-		onError: () => push("/"),
-	});
+  const [activateAccount, { loading, error, data }] = useActivateUserMutation({
+    update(cache) {
+      cache.modify({
+        id: `User:${user?.id}`,
+        fields: {
+          verified() {
+            return true;
+          },
+        },
+      });
+    },
+  });
 
-	useEffect(() => {
-		async function activateUserAcc() {
-			return await activateAccount({
-				variables: { token: query.token as string },
-			});
-		}
-		if (mount.current) return;
-		mount.current = true;
+  useEffect(() => {
+    async function activateUserAcc() {
+      return await activateAccount({
+        variables: { token: query.token as string },
+      });
+    }
+    if (mount.current) return;
+    mount.current = true;
 
-		activateUserAcc();
-	}, [activateAccount, query]);
+    activateUserAcc();
+  }, [activateAccount, query]);
 
-	if (loading) return <Loader active />;
+  if (loading) return <Loader active />;
 
-	return (
-		<Grid container centered>
-			<Grid.Row>
-				<Grid.Column style={{ maxWidth: 340 }} textAlign="center">
-					<div
-						style={{
-							width: "100%",
-							display: "flex",
-							flexDirection: "row",
-							justifyContent: "center",
-							paddingTop: "32px",
-							cursor: "pointer",
-						}}
-					>
-						<NextLink href="/">
-							<Image
-								alt="flaw-logo-notext"
-								src={logo}
-								height={48}
-								width={48}
-								priority={true}
-							/>
-						</NextLink>
-					</div>
+  return (
+    <Grid container centered>
+      <Grid.Row>
+        <Grid.Column style={{ maxWidth: 340 }} textAlign="center">
+          <div
+            style={{
+              width: "100%",
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              paddingTop: "32px",
+              cursor: "pointer",
+            }}
+          >
+            <NextLink href="/">
+              <Image
+                alt="flaw-logo-notext"
+                src={logo}
+                height={48}
+                width={48}
+                priority={true}
+              />
+            </NextLink>
+          </div>
 
-					<Header as="h2" textAlign="center">
-						{t("header")}
-					</Header>
-				</Grid.Column>
-			</Grid.Row>
-		</Grid>
-	);
+          <Header as="h2" textAlign="center">
+            {data?.activateUser ? t("header") : t("dialog.tokenExpired")}
+          </Header>
+
+          <Button
+            style={{ marginTop: "5rem" }}
+            size="big"
+            circular
+            primary
+            content={t("menu.home", { ns: "common" })}
+            as={NextLink}
+            href="/"
+          />
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  );
 };
 
 export const getStaticProps = async ({ locale }: { locale: string }) => ({
-	props: {
-		...(await serverSideTranslations(locale, ["activation"])),
-	},
+  props: {
+    ...(await serverSideTranslations(locale, ["activation", "common"])),
+  },
 });
 
 export default ActivateUserAccount;
