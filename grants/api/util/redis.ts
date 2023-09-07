e21f5e7d332e4816c2bfc6fae6116b0260e03173
Matus Muransky <matus.muransky2@gmail.com>
2023-09-07 10:35:51 +0200

    minor redis init fix

--- grants/api/util/redis.ts
+++ grants/api/util/redis.ts
@@ -8,11 +8,17 @@ export const initRedis = async () => {
 
     client.on("connect", () => console.log("Redis client connected!"));
 
-    client.on("error", (err: any) => console.log("Redis Client Error", err));
+    client.on("error", (err: any) => {
+      console.log("Redis Client Error", err);
+      // Retry after an error occurs
+      setTimeout(() => initRedis(), 1000 * 15);
+    });
 
-    return client;
+    return client; // Return the Redis client upon successful connection
   } catch (error) {
     console.log("Redis init failed, retrying in 15sec...");
     setTimeout(() => initRedis(), 1000 * 15);
+    // Return a promise that resolves to undefined in case of an error
+    return Promise.resolve(undefined);
   }
 };
