bc86c6095e15783965314699392dcaddac7aced1
Matus Muransky <matus.muransky2@gmail.com>
2023-09-21 10:04:34 +0200

    testing office365 mail server

--- mail/src/config.ts
+++ mail/src/config.ts
@@ -5,10 +5,10 @@ export default () => ({
     host: process.env.MAIL_HOST,
     port: process.env.MAIL_PORT,
     secure: process.env.NODE_ENV === 'production',
-    auth: {
-      user: process.env.MAIL_USER,
-      pass: process.env.MAIL_PASSWORD,
-    },
+    // auth: {
+    //   user: process.env.MAIL_USER,
+    //   pass: process.env.MAIL_PASSWORD,
+    // },
     // tls: {
     //   ciphers: 'SSLv3',
     // },
