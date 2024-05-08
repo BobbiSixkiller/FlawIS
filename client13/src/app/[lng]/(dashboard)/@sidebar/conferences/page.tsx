e58b4915a8e06a3cbcd35c7c95a27b653121c44f
Matus Muransky <matusmuransky@Matuss-MacBook-Pro.local>
2024-05-08 10:52:51 +0200

    sidebar fix

--- client13/src/app/[lng]/(dashboard)/@sidebar/conferences/page.tsx
+++ client13/src/app/[lng]/(dashboard)/@sidebar/conferences/page.tsx
@@ -1,9 +1,13 @@
+import { getMe } from "@/app/[lng]/(auth)/actions";
 import SearchConference from "./SearchConference";
+import { Role } from "@/lib/graphql/generated/graphql";
 
 export default async function Sidebar({
   params: { lng },
 }: {
   params: { lng: string };
 }) {
-  return <SearchConference lng={lng} />;
+  const user = await getMe();
+
+  return user?.role === Role.Admin ? <SearchConference lng={lng} /> : null;
 }
