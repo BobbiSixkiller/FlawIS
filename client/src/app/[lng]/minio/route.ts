4ec1aa2b47ba86779b0c47b1ac41e5438f13a51c
Matus Muransky <matusmuransky@uep-90235736.flaw.uniba.sk>
2024-10-08 11:42:26 +0200

    minio zip download

--- client/src/app/[lng]/minio/route.ts
+++ client/src/app/[lng]/minio/route.ts
@@ -3,6 +3,7 @@ import { downloadFile } from "@/lib/minio";
 import { NextRequest, NextResponse } from "next/server";
 import { Readable } from "stream";
 import { getMe } from "../(auth)/actions";
+import archiver from "archiver";
 
 function nodeStreamToWebReadable(stream: Readable): ReadableStream {
   return new ReadableStream({
@@ -37,31 +38,60 @@ export async function GET(
 
   const searchParams = request.nextUrl.searchParams;
   const bucketName = searchParams.get("bucketName")?.valueOf().toLowerCase();
-  const objectName = searchParams.get("objectName")?.valueOf().toLowerCase();
+  const objectNames = searchParams
+    .getAll("objectName")
+    .map((name) => name.valueOf().toLowerCase()); // Fetch all object names
 
-  if (!bucketName || !objectName) {
+  if (!bucketName || objectNames.length === 0) {
     return NextResponse.json({ message: t("400") }, { status: 400 });
   }
 
   try {
     // Download the file
-    const fileStream = await downloadFile(
-      bucketName,
-      objectName.split(`http://minio:9000/${bucketName}`)[1]
-    );
+    if (objectNames.length === 1) {
+      const fileStream = await downloadFile(
+        bucketName,
+        objectNames[0].split(`http://minio:9000/${bucketName}`)[1]
+      );
 
-    // Convert Node.js stream to web ReadableStream
-    const webReadableStream = nodeStreamToWebReadable(fileStream);
+      // Convert Node.js stream to web ReadableStream
+      const webReadableStream = nodeStreamToWebReadable(fileStream);
 
-    // Return the file stream as the response
-    return new NextResponse(webReadableStream, {
-      headers: {
-        "Content-Type": "application/octet-stream", // Set the appropriate MIME type
-        "Content-Disposition": `attachment; filename="${objectName
-          .split("-")
-          .pop()}"`, // Set the filename for download
-      },
-    });
+      // Return the file stream as the response
+      return new NextResponse(webReadableStream, {
+        headers: {
+          "Content-Type": "application/octet-stream", // Set the appropriate MIME type
+          "Content-Disposition": `attachment; filename="${objectNames[0]
+            .split("-")
+            .pop()}"`, // Set the filename for download
+        },
+      });
+    } else {
+      // Create a zip archive
+      const archive = archiver("zip", { zlib: { level: 9 } });
+
+      // Create a PassThrough stream to convert it to a web-readable stream
+      const zipStream = new Readable().wrap(archive);
+
+      // Stream the zip file back to the client
+      const webReadableStream = nodeStreamToWebReadable(zipStream);
+
+      // Add each file to the zip archive
+      for (const objectName of objectNames) {
+        const fileStream = await downloadFile(bucketName, objectName);
+        archive.append(fileStream, { name: objectName.split("-").pop()! }); // Add file with appropriate name
+      }
+
+      // Finalize the archive
+      archive.finalize();
+
+      return new NextResponse(webReadableStream, {
+        headers: {
+          "Content-Type": "application/zip",
+          "Content-Disposition": `attachment; filename="files.zip"`,
+        },
+      });
+    }
   } catch (error: any) {
     console.log(error);
     return NextResponse.json({ message: t("500") }, { status: 500 });
--- client/src/components/Breadcrumbs.tsx
+++ client/src/components/Breadcrumbs.tsx
@@ -28,12 +28,6 @@ export default function Breadcrumbs({
   return (
     <div>
       <ul className={containerClasses}>
-        {/* <li className={listClasses}>
-          <Link href={"/"} className={activeClasses}>
-            {homeElement}
-          </Link>
-        </li> */}
-        {/* {pathNames.length > 0 && separator} */}
         {pathNames.map((link, index) => {
           let href = `/${pathNames.slice(0, index + 1).join("/")}`;
           let itemClasses =
