dc9516a56eb7fd61e32b0b7849580baab2ae9d71
Matus Muransky <matusmuransky@Matuss-MacBook-Pro.local>
2025-01-09 13:46:11 +0100

    testing

--- client/src/middlewares/tenantMiddleware.ts
+++ client/src/middlewares/tenantMiddleware.ts
@@ -25,6 +25,8 @@ export function withTenant(middleware: CustomMiddleware) {
     const hostname = req.headers.get("host") || ""; // Get the hostname from the request
     const subdomain = hostname.split(".")[0]; // Parse the subdomain (assuming subdomain is the first part)
 
+    console.log(subdomain);
+
     let lng: string | undefined;
     const paths = url.pathname.split("/").filter((path) => path);
     if (paths[0]?.length === 2) {
