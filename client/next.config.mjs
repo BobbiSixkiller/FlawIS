3602c08cfb5f426f399e82838b98001f227d493f
Matus Muransky <matusmuransky@Matuss-MacBook-Pro.local>
2025-06-23 22:25:11 +0200

    testing standalone output

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -2,6 +2,7 @@ import withPlaiceholder from "@plaiceholder/next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   logging: { fetches: { fullUrl: true } },
   serverExternalPackages: ["@react-pdf/renderer"],
   experimental: {
